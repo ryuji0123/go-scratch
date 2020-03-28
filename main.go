323c47c142ec422776b21351fc8433d50324434f
ryuji0123 <ryo1011.javelin@gmail.com>
2020-03-29 01:02:22 +0900

    add main.go

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type HelloHandler struct {
+
+}
+
+func (h *HelloHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World!")
+}
+
+func main() {
+	hello:= HelloHandler{}
+	server := http.Server{
+		Addr: "127.0.0.1:8080",
+	}
+	http.Handle("/", &hello)
+	server.ListenAndServe()
+}
